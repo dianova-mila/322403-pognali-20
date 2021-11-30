db42bd00ceaf8c03ec67a4a863b47edc873200e3
Dianova Mila <dianova.mila.spb@gmail.com>
2021-11-30 23:54:09 +0300

    Добавлен результат сборки для github pages

---
+++ docs/js/header.js
@@ -0,0 +1,21 @@
+var header = document.querySelector(".header");
+var main = document.querySelector(".main");
+var headerButton = document.querySelector(".header__button");
+var headerNav = document.querySelector(".main-nav");
+
+header.classList.remove("header--nojs");
+main.classList.remove("main--nojs");
+
+headerButton.addEventListener("click", function() {
+  header.classList.toggle("header--opened");
+});
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 1) {
+    header.classList.add("header--scroll");
+    headerNav.classList.add("main-nav--scroll")
+  } else {
+    header.classList.remove("header--scroll");
+    headerNav.classList.remove("main-nav--scroll")
+  }
+});
